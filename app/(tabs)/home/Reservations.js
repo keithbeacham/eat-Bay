ad2a1e3491a5d822cd93b2daa997131852e830f4
Wiezien <145956394+Wiezien@users.noreply.github.com>
2024-05-24 12:31:20 +0100

    lunch

--- app/(tabs)/home/Reservations.js
+++ app/(tabs)/home/Reservations.js
@@ -1,23 +1,108 @@
-import React from "react";
-import { View, Text, Button } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
 import { Stack, Link, useRouter } from "expo-router";
+import Button from "../../components/Button";
+import MapView from "react-native-maps";
+import {
+  getFoodByFoodId,
+  getReservationsByUserId,
+  getShopById,
+} from "../../../src/api/backEndApi";
 
-export default function ViewTransactions() {
+export default function ViewReservations() {
+  const user_id = 1;
+  const [reservations, setReservations] = useState([]);
   const router = useRouter();
+  useEffect(() => {
+    const reservationArray = [...getReservationsByUserId(user_id)];
+    reservationArray.map((reservation) => {
+      const reservationCopy = { ...reservation };
+      const shopObject = getShopById(reservation.shop_id);
+      reservationCopy.location = shopObject.address;
+      reservationCopy.pickUpTimes = shopObject.pickup_times;
+      const foodObject = getFoodByFoodId(reservation.food_id);
+      reservationCopy.itemName = foodObject.item_name;
+      return reservationCopy;
+    });
+    console.log(reservationArray);
 
+    setReservations(reservationArray);
+  }, []);
   return (
     <>
-      <View>
-        <Stack.Screen
-          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-          options={{ headerShown: false, title: "Transaction List" }}
-        />
-        <Text>This is the "view users transactions" screen</Text>
-        <Button
+      <Stack.Screen
+        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        options={{ headerShown: true, title: "eatBay" }}
+      />
+      <MapView
+        style={styles.map}
+        provider={MapView.PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: 50.95,
+          longitude: -1.4,
+          latitudeDelta: 0.15,
+          longitudeDelta: 0.15,
+        }}
+      />
+      <View style={styles.pageContainer}>
+        <Text style={styles.bold30}>Reserved Items{"\n"}</Text>
+        <Text style={styles.bold16}>Item Name{"\n"}</Text>
+        <Text style={styles.text15}>Location{"\n"}</Text>
+        <Text style={styles.text15}>Pick Up Times{"\n"}</Text>
+        <Text style={styles.bold16}>Unique ID{"\n"}</Text>
+
+        <Button title="Delete" />
+        {/* <Button
           title="go back to food list"
           onPress={() => router.replace("/home/ViewFoodList")}
-        />
+        /> */}
       </View>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  map: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    height: "100%",
+  },
+  pageContainer: {
+    position: "absolute",
+    top: "5%",
+    left: "10%",
+    width: "80%",
+    height: "90%",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(255,255,255,0.8)",
+    padding: 10,
+    borderRadius: 10,
+  },
+  foodItem: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text15: {
+    fontSize: 15,
+  },
+  bold30: {
+    fontWeight: "bold",
+    fontSize: 30,
+  },
+  bold16: {
+    fontWeight: "bold",
+    fontSize: 16,
+    textAlign: "center",
+  },
+  container: {},
+  image: {
+    // flex: 1,
+    width: 50,
+    height: 50,
+    backgroundColor: "#0553",
+  },
+});
---
+++ data/test-data/reservations.js
@@ -0,0 +1,34 @@
+module.exports = [
+  {
+    transaction_id: 1,
+    email: "sofe@northcoders.com",
+    shop_id: 2,
+    food_id: 3,
+    status: "Pending collection",
+    user_id: 1,
+  },
+  {
+    transaction_id: 2,
+    email: "sofe@northcoders.com",
+    shop_id: 2,
+    food_id: 1,
+    status: "Pending collection",
+    user_id: 1,
+  },
+  {
+    transaction_id: 3,
+    email: "marcus@northcoders.com",
+    shop_id: 1,
+    food_id: 2,
+    status: "Pending collection",
+    user_id: 2,
+  },
+  {
+    transaction_id: 4,
+    email: "keith@northcoders.com",
+    shop_id: 3,
+    food_id: 4,
+    status: "Pending collection",
+    user_id: 3,
+  },
+];
