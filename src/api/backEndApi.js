04b7c76ace5cd19f55342d3d8aee12e57cffa916
Keith <keith@beachams.net>
2024-05-29 16:22:19 +0100

    more work on notifications

--- src/api/backEndApi.js
+++ src/api/backEndApi.js
@@ -114,7 +114,6 @@ export function postFollowers(user_id, shop_id) {
   });
 }
 
-
 export function postUser(user_id, name, password) {
   const body = { user_id, name, password };
   return eatbayApi.post(`/users/${user_id}`, body).then((response) => {
@@ -134,8 +133,8 @@ export function patchUserById(user_id, name, password, push_token) {
   if (push_token) {
     body.push_token = push_token;
   }
-  return eatbayApi.patch(`/users/${user_id}`, body)
-  .then((response) => {
-    response.data.user
-  })
+  console.log("patchUserById>", body);
+  return eatbayApi.patch(`/users/${user_id}`, body).then((response) => {
+    response.data.user;
+  });
 }
